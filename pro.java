142932276bcae35ddbdefeff4522a03c93453500
JayapradhaJayaraman <127107646+JayapradhaJayaraman@users.noreply.github.com>
2023-09-30 21:46:51 +0530

    Add files via upload

---
+++ pro.java
@@ -0,0 +1,10 @@
+package mypackage;
+
+public class pro {
+	 final static String collegename = "joseph";
+	
+	
+	public static void main(String[] args) {
+		System.out.println(Math.pow(4,2));
+	}
+}
